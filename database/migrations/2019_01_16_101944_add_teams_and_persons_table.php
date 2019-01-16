5c8db7524beb78890cee2ccc19e5f0e025fb072b
Martin Henriksen <martin.henriksen.92@gmail.com>
2019-01-16 11:23:24 +0100

    add team and person tables to the database

---
+++ database/migrations/2019_01_16_101944_add_teams_and_persons_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTeamsAndPersonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+
+            $table->timestamps();
+        });
+
+        Schema::create('persons', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->float('experience');
+
+            $table->integer('team_id')->unsigned();
+            $table->foreign('team_id')->references('id')->on('teams');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teams');
+    }
+}
